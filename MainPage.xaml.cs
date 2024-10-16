61a0e406883cab828cbd737386bcd11587f2785b
Oroprogamer23 <pangilinanderick100@gmail.com>
2024-10-16 18:42:22 +0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Module07DataAccess.Services;
+using MySql.Data.MySqlClient;
+
+namespace Module07DataAccess
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly DatabaseConnectionService _dbConnectionService;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //This will initialize the database connection
+            _dbConnectionService = new DatabaseConnectionService();
+        }
+
+        private async void OnTestConnectionClicked(object sender, EventArgs e)
+        {
+            var connectionString = _dbConnectionService.GetConnectionString();
+            try
+            {
+                using (var connection = new MySqlConnection(connectionString)) 
+                { 
+                    await connection.OpenAsync();
+                    ConnectionStatusLabel.Text = "Connection Successful!";
+                    ConnectionStatusLabel.TextColor = Colors.Green;
+                }
+            }
+            catch(Exception ex)  
+            {
+                ConnectionStatusLabel.Text = $"Connection Failed: {ex.Message}";
+                ConnectionStatusLabel.TextColor = Colors.Red;
+            }
+        }
+
+        private async void OpenViewPersonal(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//ViewPersonal");
+        }
+
+    }
+
+}
---
+++ Services/PersonalService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Module07DataAccess.Model;
+using System.Data;
+
+namespace Module07DataAccess.Services
+{
+    public class PersonalService
+    {
+        private readonly string _connectionString;
+
+        public PersonalService()
+        {
+            var dbService = new DatabaseConnectionService();
+            _connectionString = dbService.GetConnectionString();
+        }
+
+
+        //Retrieves all data
+        public async Task<List<Personal>> GetAllPersonalsAsync()
+        {
+            var personalService = new List<Personal>();
+
+            // Correct instantiation of MySqlConnection
+            using (var conn = new MySqlConnection(_connectionString))
+            {
+                await conn.OpenAsync();
+                var cmd = new MySqlCommand("SELECT * FROM tblEmployee", conn);
+
+                using (var reader = await cmd.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        personalService.Add(new Personal
+                        {
+                            EmployeeID = reader.GetInt32("EmployeeID"),
+                            Name = reader.GetString("Name"),
+                            Address = reader.GetString("Address"),
+                            email = reader.GetString("email"),
+                            ContactNo = reader.GetString("ContactNo"),
+                        });
+                    }
+                }
+                return personalService;
+            }
+        }
+    }
+}
